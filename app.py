7b2d9bb7831d0b659adef0ecd56a968a82267ffc
yushi-forest <ymori8143@gmail.com>
2025-10-16 19:22:54 +0900

    p.65

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, url_for,request, current_app, g, redirect
+from email_validator import validate_email, EmailNotValidError
+
+from flask import (
+        Flask, 
+        render_template, 
+        url_for,
+        request, 
+        current_app, 
+        g, 
+        redirect,
+        flash,
+)
+
+
 
 app = Flask(__name__)
+#SERCH_KEYを追加する
+app.congfig["SERCH_KEY"] = "2AZSMss3p5QPbcY2hBs"
 
 @app.route("/")
 def index():
@@ -28,7 +43,7 @@ with app.test_request_context():
 
 #アプリケーションコンテキストを取得してスタックにpushする
 ctx = app.app_context()
-ctx.push()
+cx.push()
 
 with app.test_request_context("/uses?updated=ture"):
     #tureが出力される
@@ -41,9 +56,40 @@ def contact():
 @app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
+        #form属性を使って、フォーム値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        #入力チェック
+        is_valid = Ture
+
+        if not username:
+            flash("ユーザー名は必須です"）
+            is_valid = Flase
+
+        if not email:
+                  flash("メールアドレスは必須です")
+                  is_vaild = Flase
+
+        try:
+                  validate_email(email)
+        except EmailNotValidError;
+                  flash("メールアドレスの形式で入力してください")
+                  is_valid = Flase
+
+        if not description:
+                  flash("お問い合わせ内容は必須です")
+                  is_valid = Flase
+
+        if is _valid:
+                  return redirect(url_for("contact"))
+                  
+
         #メールを送る
 
        #contactエンドポイントへリダイレクトする
+        flash("お問い合わせ内容はメールにて送信いたしました。お問い合わせありがとうございます。")
        return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
